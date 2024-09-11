8105b32a5180bf1a5f31b1713eb3912937141bcd
Son Dao <udemydaohsn@gmail.com>
2024-09-11 11:11:15 -0700

    reconfigure prisma connect

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
-// src/app/page.tsx
+import { prisma } from "@/lib/prisma";
 
-// import { prisma } from "@/lib/prisma";
+const Page = async () => {
+  const users = await prisma.user.findMany();
 
-// const Home = async () => {
-//   const users = await prisma.user.findMany();
+  return (
+    <div className="p-4 flex flex-col gap-y-4">
+      <h2>Home</h2>
 
-//   return (
-//     <div className="p-4 flex flex-col gap-y-4">
-//       <h2>Home</h2>
-
-//       <ul className="flex flex-col gap-y-2">
-//         {users.map((user) => (
-//           <li key={user.id}>{user.firstName}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-import React from "react";
-
-const Page = () => {
-  return <div>Page</div>;
+      <ul className="flex flex-col gap-y-2">
+        {users.map((user) => (
+          <li key={user.id}>{user.firstName}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Page;
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const prismaClientSingleton = () => {
-//   return new PrismaClient();
-// };
-
-// declare global {
-//   // eslint-disable-next-line no-var
-//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-// }
-
-// export const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+export const prisma = new PrismaClient();
